a9bacc691ee35a933a289f017884bb26bdf3bf8b
Rutuj0703 <rutujkoli7@gmail.com>
2025-01-23 14:04:24 +0530

    captain signup and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,41 @@
 import React, { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignUp from './pages/UserSignUp'
 import CaptainLogin from './pages/CaptainLogin'
 import CaptainSignUp from './pages/CaptainSignUp'
 import { UserDataContext } from './context/userContext'
+import Start from './pages/Start'
+import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import CaptainHome from './pages/CaptainHome'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Start />} />
         <Route path='/login' element={<UserLogin />} />
         <Route path='/signup' element={<UserSignUp />} />
         <Route path='/captain-login' element={<CaptainLogin />} />
         <Route path='/captain-signup' element={<CaptainSignUp />} />
+        <Route path='/home' element={
+          <UserProtectWrapper>
+              <Home />
+          </UserProtectWrapper>
+          } />
+          <Route path='/user/logout' element={
+            <UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          }></Route>
+        <Route path='/captain-home' element={
+          <CaptainProtectWrapper>
+              <CaptainHome/>
+          </CaptainProtectWrapper>
+          } />
       </Routes>
     </div>
   )
--- frontend/src/pages/UserSignUp.jsx
+++ frontend/src/pages/UserSignUp.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import userContext, { UserDataContext } from '../context/userContext'
+
 
 const UserSignUp = () => {
   const[ email,setEmail ] = useState('')
@@ -8,17 +11,30 @@ const UserSignUp = () => {
   const[ lastName,setLastName ] = useState('')
   const[ userData,setUserData ] = useState({})
 
-  const submitHandler=(e)=>{
+  const navigate= useNavigate()
+
+  const {user,setUser} = useContext(UserDataContext)
+
+  const submitHandler= async (e)=>{
     e.preventDefault();
-    setUserData({
-      fullName:{
-        firstName:firstName,
-        lastName:lastName
+    const newUser= {
+      fullname:{
+        firstname:firstName,
+        lastname:lastName
       },
       email:email,
       password:password
-    
-    })
+    }
+
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/users/register`, newUser)
+
+    if(response.status === 201){
+      const data=response.data
+      setUser(data.user)
+      localStorage.setItem('token', data.token)
+      navigate('/home')
+    }
+
     setEmail('')
     setPassword('')
     setFirstName('')
